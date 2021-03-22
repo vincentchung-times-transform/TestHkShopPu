3b43a2e9e80487af0304635a9502da51b0e650cb
vincentchung-times-transform <vincentchung@times-transform>
2021-03-22 18:55:52 +0800

    multi pics func and inventory func done

--- app/src/main/java/com/hkshopu/hk/data/bean/MyAddresssItem.kt
+++
@@ -1,5 +0,0 @@
-package com.hkshopu.hk.data.bean
-
-data class MyAddresssItem(val name:String,
-                          val phone:String, val address:String,val status:String) {}
-
--- app/src/main/java/com/hkshopu/hk/ui/main/fragment/SecondFragment.kt
+++ app/src/main/java/com/hkshopu/hk/ui/main/fragment/SecondFragment.kt
@@ -1,25 +1,13 @@
 package com.hkshopu.hk.ui.main.fragment
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.hkshopu.hk.R
-import com.hkshopu.hk.component.EventShopNameUpdated
-import com.hkshopu.hk.databinding.ActivityEmailverifyBinding
-import com.hkshopu.hk.databinding.FragmentSecondBinding
-import com.hkshopu.hk.databinding.FragmentShopinfoBinding
-import com.hkshopu.hk.ui.main.activity.MyAddressActivity
-import com.hkshopu.hk.ui.user.activity.TermsOfServiceActivity
-import com.hkshopu.hk.ui.user.activity.UserIofoActivity
-import com.hkshopu.hk.utils.rxjava.RxBus
 
-class SecondFragment : Fragment(), View.OnClickListener {
-
+class SecondFragment : Fragment() {
     companion object {
         fun newInstance(): SecondFragment {
             val args = Bundle()
@@ -28,29 +16,12 @@ class SecondFragment : Fragment(), View.OnClickListener {
             return fragment
         }
     }
-
-    val view = ""
-
-    private var binding: FragmentSecondBinding? = null
-    private var fragmentShopInfoBinding: FragmentSecondBinding? = null
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_second, container, false)
-        view.findViewById<TextView>(R.id.txtView_more_myaddress).setOnClickListener(this)
-        return view
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
-    override fun onClick(view: View) {
-        when (view.id) {
-            R.id.txtView_more_myaddress -> {
-                val intent = Intent(this.context, MyAddressActivity::class.java)
-                startActivity(intent)
-
-            }
-        }
-    }
 }
